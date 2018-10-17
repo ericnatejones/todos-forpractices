e59009736c89310b3a9b6d9e9f61b134e92d5fae
Bob Ziroll <elderziroll@gmail.com>
2018-10-17 17:20:12 -0600

    Feedback
    Great job! Check out the changes I proposed.

--- src/Components/Todos/Todo.js
+++ src/Components/Todos/Todo.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+// This file has no real code in it. Either delete it or add something.
 export default class Todo extends Component {
   render() {
     return (
